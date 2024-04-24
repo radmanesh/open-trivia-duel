8784a72a20c04db949d9b5443e206b79f5056b66
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-04-24 03:07:23 +0200

    refactor: Get game date from context

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,28 +1,25 @@
 "use client";
 
+import Quiz from "./quiz";
 import { Button } from "@/components/ui/button";
 import { useQuestions } from "@/services/use-questions";
 import { CircleAlertIcon, CircleEllipsisIcon } from "lucide-react";
-import { useSearchParams } from "next/navigation";
-import Quiz from "./quiz";
-import { Suspense } from "react";
+import { useGameContext } from "@/contexts/game-provider";
 
 export default function QuizPage() {
-  const params = useSearchParams();
+  const { game } = useGameContext();
 
   const { data, isError, isLoading } = useQuestions({
-    amount: Number(params.get("amount")),
-    category: Number(params.get("category")),
-    difficulty: params.get("difficulty") as "easy" | "medium" | "difficult",
+    amount: Number(game.questionsPerRound),
+    category: Number(game.nextRound.categoryId),
+    difficulty: game.level,
   });
 
   if (isLoading) {
     return (
       <main className="flex min-h-screen flex-col items-center justify-center space-y-3 bg-slate-50/95">
-        <CircleEllipsisIcon className="text-blue-500/95 h-10 w-10 animate-spin" />
-        <p className="text-blue-500/95">
-          Loading questions for round {params.get("round")}
-        </p>
+        <CircleEllipsisIcon className="text-primary/95 h-10 w-10 animate-spin" />
+        <p className="text-primary/95 font-bold">Loading questions</p>
       </main>
     );
   }
@@ -39,9 +36,5 @@ export default function QuizPage() {
     );
   }
 
-  return (
-    <Suspense>
-      <Quiz questions={data} />;
-    </Suspense>
-  );
+  return <Quiz questions={data} />;
 }
