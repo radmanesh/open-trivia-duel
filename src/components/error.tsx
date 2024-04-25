96250eb84d3c731ef536c7fb7e198bde9d6d1b9f
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-04-25 21:25:18 +0200

    fix: Change error button variant to destructive

--- src/components/error.tsx
+++ src/components/error.tsx
@@ -14,7 +14,9 @@ export function PageError({ errorMessage, onClickTryAgain }: ErrorProps) {
     <main className="flex min-h-screen flex-col items-center justify-center space-y-3 bg-slate-50/95">
       <CircleAlertIcon className="text-red-500/95 h-10 w-10" />
       <p className="text-red-500/95">{errorMessage}</p>
-      <Button onClick={onClickTryAgain}>Try again</Button>
+      <Button variant="destructive" onClick={onClickTryAgain}>
+        Try again
+      </Button>
     </main>
   );
 }
